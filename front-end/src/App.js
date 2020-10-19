3469603c6b8dc189c153b388afcbee5759523908
Brenson <brensonw300@gmail.com>
2020-10-19 09:59:56 -0700

    Added code to potlucklistall potluckattending and app js files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/Login';
+import UserDashboard from './Private/UserDashboard';
+import Private from './utils/PrivateRoute';
+import Register from './components/Register';
 
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Plan A Potluck</h1>
+          <Route exact path="/" component={Login} />
+        </header>
+        <Register />
+        <div className="attendee">
+          <Private path="/protected" component={UserDash} />
+        {/* <Potluck /> */}
+        </div>
+      </div>
+    </Router>
   );
 }
 
