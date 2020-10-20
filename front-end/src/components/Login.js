b6a1cc78499693e2c6e3992e824f27f84ce173c5
justin-mavity <justinmavity1021@gmail.com>
2020-10-20 11:00:12 -0500

    Login complete

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const { values, submit, update } = props;
+  const onChange = (evt) => {
+    const { name, value } = evt.target;
+    update(name, value);
+  };
 
-export default Login
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+    submit();
+  };
+
+  return (
+    <div className="login form">
+      <header>
+        <nav className="links">
+          <a href="/home">Home</a>
+          <a href="/signup">Sign up!</a>
+        </nav>
+      </header>
+      <div className="login inputs">
+        <form className="form container">
+          <label>
+            Username:
+            <input
+              type="text"
+              name="username"
+              value={values.username}
+              onChange={onChange}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={values.password}
+              onChange={onChange}
+            />
+          </label>
+          <button>Submit</button>
+          <button>Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
