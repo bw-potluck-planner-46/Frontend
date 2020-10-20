4e9887cfa8cdc658913033fa7d66657bfa70421c
unknown <tompsherman@gmail.com>
2020-10-20 16:34:05 -0400

    organizer start page edits

---
+++ front-end/src/components/OrganizerStart.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {useHistory, Switch, Route} from 'react-router-dom'
+
+const OrganizerStart = () => {
+    const history = useHistory()
+
+    const loginRoute = () => {
+        history.push(`/organizer/login`)
+    }
+    const registerRoute = () => {
+        history.push(`/organizer/register`)
+    }
+    return (
+        <div>
+            <h2>Welcome Organizer!</h2>
+            <button onClick={loginRoute}>Log In</button>
+            <button onclick={registerRoute}>Register</button>
+            <Switch>
+            <Route exact path="/organizer/login" component={Login} />
+            <Route exact path="/organizer/register" component={Register} />
+        </Switch>
+        </div>
+    )
+}
+
+export default OrganizerStart
