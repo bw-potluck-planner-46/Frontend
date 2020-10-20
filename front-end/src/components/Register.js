e54d75e1d9a4c7e0c72c0ca879bc25ce4574ddad
djviodes <djviodes26@gmail.com>
2020-10-20 09:19:10 -0700

    Styled Register

--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -1,6 +1,48 @@
 
 import React, { useState, useEffect } from 'react';
 import * as yup from 'yup';
+import styled from 'styled-components'
+
+const StyledFormContainer = styled.form`
+    display: flex;
+    justify-content: center;
+`
+
+const StyledForm = styled.form`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const StyledLabel = styled.label`
+    margin-bottom: 2%;
+`
+
+const StyledInput = styled.input`
+    width: 50%;
+    height: 3.5vh;
+    padding: 0 1.75%;
+    border: 1px solid grey;
+    border-radius: 50px;
+    outline: none;
+    font-size: 1rem;
+    margin-bottom: 2%;
+`
+
+const StyledButton = styled.button`
+    margin-top: 2%;
+    font-size: 1rem;
+    width: 10%;
+    height: 3vh;
+    background-color: #867EBA;
+    border: none;
+    border-radius: 50px;
+    outline: none;
+    &:hover{
+        cursor: pointer;
+    }
+`
 
 const formSchema = yup.object().shape({
     username: yup.string().required('Please enter a username').min(5, 'Must have at least 5 characters').max(15, 'May not be longer than 15 characters'),
@@ -59,61 +101,65 @@ const Register = () => {
     };
 
     return (
-        <form>
-            <label htmlFor='username'>Username:</label>
-            <input 
-                id='username' 
-                name='username' 
-                type='text' 
-                placeholder='Username'
-                value={user.username}
-                onChange={changeHandler}
-            />
-            {errorState.username.length > 0 ? <p>{errorState.username}</p> : null}
+        <StyledFormContainer>
+            <StyledForm>
+                <StyledLabel htmlFor='username'>Username</StyledLabel>
+                <StyledInput 
+                    id='username' 
+                    name='username' 
+                    type='text' 
+                    placeholder='Username'
+                    value={user.username}
+                    onChange={changeHandler}
+                />
+                {errorState.username.length > 0 ? <p>{errorState.username}</p> : null}
 
-            <label htmlFor='email'>Email:</label>
-            <input 
-                id='email'
-                name='email'
-                type='email'
-                placeholder='Email'
-                value={user.email}
-                onChange={changeHandler}
-            />
-            {errorState.email.length > 0 ? <p>{errorState.email}</p> : null}
+                <StyledLabel htmlFor='email'>Email</StyledLabel>
+                <StyledInput 
+                    id='email'
+                    name='email'
+                    type='email'
+                    placeholder='Email'
+                    value={user.email}
+                    onChange={changeHandler}
+                />
+                {errorState.email.length > 0 ? <p>{errorState.email}</p> : null}
 
-            <label htmlFor='password'>Password:</label>
-            <input 
-                id='password'
-                name='password'
-                type='text'
-                placeholder='Password'
-                value={user.password}
-                onChange={changeHandler}
-            />
-            {errorState.password.length > 0 ? <p>{errorState.password}</p> : null}
-            
-            <input 
-                id='organizer' 
-                name='role' 
-                type='radio' 
-                value='Organizer' 
-                onChange={changeHandler} 
-            />
-            <label htmlFor='organizer'>Organizer</label>
+                <StyledLabel htmlFor='password'>Password</StyledLabel>
+                <StyledInput 
+                    id='password'
+                    name='password'
+                    type='text'
+                    placeholder='Password'
+                    value={user.password}
+                    onChange={changeHandler}
+                />
+                {errorState.password.length > 0 ? <p>{errorState.password}</p> : null}
+                
+                <div>
+                    <input 
+                        id='organizer' 
+                        name='role' 
+                        type='radio' 
+                        value='Organizer' 
+                        onChange={changeHandler} 
+                    />
+                    <label htmlFor='organizer'>Organizer</label>
 
-            <input 
-                id='guest' 
-                name='role' 
-                type='radio' 
-                value='Guest' 
-                onChange={changeHandler} 
-            />
-            <label htmlFor='guest'>Guest</label>
-            {errorState.role.length > 0 ? <p>{errorState.role}</p> : null}
+                    <input 
+                        id='guest' 
+                        name='role' 
+                        type='radio' 
+                        value='Guest' 
+                        onChange={changeHandler} 
+                    />
+                    <label htmlFor='guest'>Guest</label>
+                </div>
+                {errorState.role.length > 0 ? <p>{errorState.role}</p> : null}
 
-            <button type='submit' disabled={disabled}>Submit</button>
-        </form>
+                <StyledButton type='submit' disabled={disabled}>Submit</StyledButton>
+            </StyledForm>
+        </StyledFormContainer>
     );
 };
 
