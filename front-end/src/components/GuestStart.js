2a952e9d7e73699fd24ee4c7dee606425d96dfb9
unknown <tompsherman@gmail.com>
2020-10-20 17:23:47 -0400

    guest login & register routing set up

--- front-end/src/components/GuestStart.js
+++ front-end/src/components/GuestStart.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import { Switch, useHistory, Route } from 'react-router-dom'
+import Login from "./Login"
+import Register from "./Register"
 
 const GuestStart = () => {
+    const history = useHistory()
+
+    const guestLogin = () => {
+        history.push(`/guest/login`)
+    }
+    const guestRegister = () => {
+        history.push(`/guest/register`)
+    }
+
     return (
-        <div>
-            <h2>Welcome Guest!</h2>
-            <button>Log In</button>
-            <button>Register</button>
-        </div>
+        <>
+            <div>
+                <h2>Welcome Guest!</h2>
+                <button onClick={guestLogin}>Log In</button>
+                <button onClick={guestRegister}>Register</button>
+            </div>
+            <Switch>
+                <Route path="/guest/login">
+                    <Login />
+                </Route>
+                <Route path="/guest/register">
+                    <Register />
+                </Route>
+            </Switch>
+        </>
     )
 }
 
