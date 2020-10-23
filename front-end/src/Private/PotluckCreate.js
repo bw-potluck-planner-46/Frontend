5f226ff10f63695f95a8f3fad0b1d4f0570493a3
justin-mavity <justinmavity1021@gmail.com>
2020-10-23 02:59:37 -0500

    finished styling

--- front-end/src/Private/PotluckCreate.js
+++ front-end/src/Private/PotluckCreate.js
@@ -1,57 +1,126 @@
 import React, { useState, useEffect} from "react";
 import axios from 'axios';
 import styled from 'styled-components';
-import { v4 as uuidv4 } from 'uuid';
 import * as yup from "yup";
 
 const StyledContainer = styled.form`
-  dispaly:flex;
-  justify-content: center;
+display: flex;
+justify-content: center;
+background-color: #867EBA;
+padding-bottom: 30%;
+padding-top:10%;
 `
 const StyledForm = styled.form`
-  width: 50%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+width: 60%;
+display: flex;
+flex-direction: column;
+background-color: lightgoldenrodyellow;
+
+`
+const StyledFormInputs = styled.div`
+  width:100%;
+  display:flex;
+  flex-direction:column;
+  color:chocolate;
+  font-size:2rem;
+  h2{
+    font-size:4.5rem;
+    text-align:center;
+    background-color: #867EBA;
+    color:lightgoldenrodyellow;
+    padding-bottom:2%;
+  }
 `
 const StyledLabel = styled.label`
-  margin-bottom: 2%;
+display:flex;
+width: 8%;
 `
 const StyledInputContainer = styled.div`
-  width: 100%;
+  width: 100%;;
   display: flex;
   flex-direction: row;
   justify-content: center;
+  margin:1% 0;
 `
 const StyledInputs = styled.input`
-  width: 50%;
-  height: 3.5vh;
+width: 90%;
+margin:1%;
+height: 3.5vh;
+border: 1px solid grey;
+border-radius: 50px;
+outline: none;
+font-size: 1rem;
+dispaly:flex;
+justify-content:center;
+align-items:center;
 `
 const StyledTime = styled.div`
   display:flex;
-  
+  margin:.7%;
+  .input{
+    width:30%;
+    justify-content:flex-start;
+  }
 `
-const StyledErrorsContainer = styled.div``
-const StyledList = styled.div``
+const StyledErrorsContainer = styled.div`
+font-size: 0.75rem;
+color: #DA3436;
+`
 const StyledButton = styled.button`
-  font-size: 1rem;
-  width: 10%;
-  background-color: #867EBA;
-  &:hover{
+margin:2% 0 0 45%;
+font-size: 2rem;
+align-items:center;
+justify-content:center;
+width: 10%;
+height: 6vh;
+background-color: #867EBA;
+border: none;
+border-radius: 50px;
+outline: none;
+&:hover{
     cursor: pointer;
+    background-color: #7E7E7E;
+    transform: scale(1.1);
 }
 `
-const StyledError = styled.p`
-  font-size: 0.75rem;
-  color: #DA3436;
+const StyledButtonContainer = styled.div`
+width:100%;
 `
+const StyledItemContainer = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  margin-left:3%;
+`
+
+const StyledItem = styled.div`
+width:100%;
+display:flex;
+flex-directiom:column;
+justify-content:center;
+align-items:center;
+
+`
+
+const StyledAddItem = styled.div`
+width:35%;
+dispaly:flex;
+`
+
+const StyledItemList = styled.div`
+width:55%;
+dispaly:flex;
+justify-items:center;
+align-items:center;
+`
+
 
 const formSchema = yup.object().shape({
   name: yup.string().required("event name is required").min(2,'name must be at least 2 characters long'),
   location: yup.string().required("event location is required").min(2,'location must be at least 2 characters long'),
   date: yup.string().required("event date is required").max(8,'date cant not be more than 8 characters'),
   time: yup.string().required("event start time is required").max(4,'characters long'),
-  meridiem: yup.string().required().oneOf(["a.m.", "p.m."]),
+  meridiem: yup.string().oneOf(["A.M", "P.M"]),
   items: yup.string(),
 });
 
@@ -81,46 +150,10 @@ const itemList = [
 const initialPotlucks = [];
 
 const PotluckCreate = (props) => {
-  const{items} = props;
-  const [potlucks, setPotlucks] = useState(initialPotlucks);
-<<<<<<< HEAD
-  const [formValues, setFormValues] = useState(initialFormValues);
-=======
-  const [formValues, setFormValues] = useState({
-    name: "",
-    location: "",
-    date: "",
-    time: "",
-    meridiem:'',
-    items: [],
-  });
-    
-    const handleCommaSeparatedChange = evt => {
-        const { value } = evt.target
-        setItems({
-            commaSeparated: value,
-            multiline: value
-                .split(',')
-                .map(v => v.trim())
-                .filter(Boolean)
-            .join('\n'),
-        })
-    }
-        const handleMultilineChange = evt => {
-        const { value } = evt.target
-        setItems({
-            commaSeparated: value,
-            multiline: value
-                .split(',')
-                .map(v => v.trim())
-                .filter(Boolean)
-            .join('\n'),
-        })
-    }
-  const { url, path } = useParams();
 
-  const [itemInput, setItemInput] = useState('');
->>>>>>> b72268650c7e357e92b1f0b8df359e00aef71487
+  const [potlucks, setPotlucks] = useState(initialPotlucks);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
   const [formErrors, setFormErrors]= useState(initialFormErrors);
   const [name, setName] = useState('');
   const [potluckItem, setPotluckItems] = useState(itemList);
@@ -146,6 +179,7 @@ const PotluckCreate = (props) => {
     .catch((err)=>{
       console.log(err);
     })
+    postNewPotluck(newPotluck);
   }
 
   useEffect(()=>{
@@ -179,13 +213,21 @@ const itemChange = (evt)=>{
   setName(evt.target.value)
 }
 
-  const addItem =() =>{
+  const addItem = (evt) =>{
+    evt.preventDefault();
     const newList = potluckItem.concat({name});
     setPotluckItems(newList);
     setName('');
   }
  
-
+  const formSubmit=()=>{
+    setPotluckItems({...potluckItem.split(',')
+    .map(v=>v.trim())
+    .filter(Boolean)
+    .join('\n')
+  });
+  }
+ 
   if (!potlucks) {
     return <div>Loading potluck information...</div>;
   }
@@ -193,16 +235,19 @@ const itemChange = (evt)=>{
   return (
     <StyledContainer>
     <StyledForm>
+    <StyledFormInputs>
       <h2>Create a Potluck</h2>
       <StyledErrorsContainer>
         <div>{formErrors.name}</div>
         <div>{formErrors.location}</div>
         <div>{formErrors.date}</div>
         <div>{formErrors.time}</div>
-        <div>{formErrors.meridiem}</div>
+        
       </StyledErrorsContainer>
-      <StyledLabel htmlfor='name'>Name:</StyledLabel>
       <StyledInputContainer>
+      <StyledLabel htmlfor='name'>
+        Name:</StyledLabel>
+
         <StyledInputs
           type="text"
           id='name'
@@ -212,8 +257,10 @@ const itemChange = (evt)=>{
           onChange={onChange}
         />
         </StyledInputContainer>
-      <StyledLabel htmlfor='location'>Location:</StyledLabel>
-      <StyledInputContainer>  
+        <StyledInputContainer>
+      <StyledLabel htmlfor='location'>
+        Location:</StyledLabel>
+ 
       <StyledInputs
           id='location'
           name='location'
@@ -223,8 +270,10 @@ const itemChange = (evt)=>{
           onChange={onChange}
         />
         </StyledInputContainer>
-      <StyledLabel htmlfor='date'>Date:</StyledLabel>
-      <StyledInputContainer>
+        <StyledInputContainer>
+      <StyledLabel htmlfor='date'>
+        Date:</StyledLabel>
+
         <StyledInputs 
           id='date'
           name='date'
@@ -234,9 +283,11 @@ const itemChange = (evt)=>{
           onChange={onChange}
         />
         </StyledInputContainer>
-      <StyledLabel htmlfor='time'>Time:</StyledLabel>
-      <StyledInputContainer>
-        <StyledInputs 
+
+        <StyledTime>
+      <StyledLabel htmlfor='time'>
+        Time:</StyledLabel>
+        <input 
           id='time'
           name='time'
           type='text'
@@ -244,42 +295,52 @@ const itemChange = (evt)=>{
           value={potlucks.time}
           onChange={onChange}
         />
-        </StyledInputContainer>
-        <StyledTime>
-          <StyledLabel>Meridiem</StyledLabel>
+        <br/>
           <select onChange={onChange} value={potlucks.meridiem} name='meridiem'>
-            <option value=""></option>
             <option value="a.m">A.M</option>
             <option value="p.m">P.M</option>
           </select>
         </StyledTime>
-
+        <br/>
+        <StyledItemContainer>
       <StyledLabel htmlfor='items'>
-      Suggested Items:
+      Wanted Items List:
       </StyledLabel>
-
+      
+      <StyledItem>
+      <StyledAddItem>
       <AddItem
         name={name}
         onChange={itemChange}
         onAdd={addItem}
       />
+      </StyledAddItem>
+      <StyledItemList>
       <ItemList
         potluckItem={potluckItem}
       />
+      </StyledItemList>
+      </StyledItem>
+      </StyledItemContainer>
+      <StyledButtonContainer>
+      <StyledButton type='button' onClick={onChange}>Create</StyledButton>
+      </StyledButtonContainer>
 
-      <StyledButton type='button' onClick={onChange}>Craete</StyledButton>
+      </StyledFormInputs>
      </StyledForm>
     </StyledContainer>
   );
 };
 
 const AddItem = ({name, onChange, onAdd})=>(
-  <div>
+  <div className='AddItem-container'>
+  <div className='input container'>
     <input 
     type="text"
     value={name}
     onChange={onChange}
     />
+    </div>
     <button type='button' onClick={onAdd}>Add Item</button>
   </div>
 );
